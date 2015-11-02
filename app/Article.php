d376b1fe0b9fa358965cbfd650277fab7e1b58db
Marco <marcoperafan@gmail.com>
2015-11-02 18:35:23 -0500

    Relaciones

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+     protected $table = "articles";
+    
+     protected $fillable = ['title','content', 'user_id', 'category_id', 'path', 'tags'];
 }
+
+public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+
+
